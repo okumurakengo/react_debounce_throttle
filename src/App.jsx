121565bf5589c8e3a8be96e3e20b81ea4e578e28
okumurakengo <okumurakengo1a@gmail.com>
2019-10-20 16:42:47 +0900

    tameshi

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDebounce } from "use-debounce";
+import { useThrottle } from "use-throttle";
 
 const App = () => {
-  return <>hello</>;
+  const [text, setText] = useState("");
+  const [debounceValue] = useDebounce(text, 1000);
+  const throttledText = useThrottle(text, 1000);
+
+  return (
+    <>
+      <input onChange={e => setText(e.target.value)} />
+      <p>Actual value: {text}</p>
+      <p>Debounce value: {debounceValue}</p>
+      <p>Throttle value: {throttledText}</p>
+    </>
+  );
 };
 
 export default App;
